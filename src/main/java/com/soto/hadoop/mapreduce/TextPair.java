2e23b08506346181b18e0f7efee8c0cbdeb14375
sotowang <sotowang@qq.com>
2018-09-15 22:07:26 +0800

    编写Writable类,实现WritableComparable接口,定制一个TextPair类

---
+++ src/main/java/com/soto/hadoop/mapreduce/TextPair.java
@@ -0,0 +1,78 @@
+package com.soto.hadoop.mapreduce;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class TextPair implements WritableComparable<TextPair> {
+
+    private Text first;
+    private Text second;
+
+    public TextPair() {
+        set(new Text(), new Text());
+    }
+    public TextPair(String first, String second) {
+        set(new Text(first), new Text(second));
+    }
+
+    public TextPair(Text first, Text second) {
+        set(first, second);
+    }
+
+    public void set(Text first, Text second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Text getFirst() {
+        return first;
+    }
+
+    public Text getSecond() {
+        return second;
+    }
+
+    @Override
+    public int compareTo(TextPair textPair) {
+        int cmp = first.compareTo(textPair.first);
+        if (cmp != 0) {
+            return cmp;
+        }
+        return second.compareTo(textPair.first);
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        first.write(dataOutput);
+        second.write(dataOutput);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        first.readFields(dataInput);
+        first.readFields(dataInput);
+    }
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() * 163 + second.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof TextPair) {
+            TextPair tp = (TextPair) o;
+            return first.equals(tp.first) && second.equals(tp.second);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return first + "\t" + second;
+    }
+}
